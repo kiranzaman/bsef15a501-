2e6a94b557c2fac06cc5b73711ee9db5ca8386b8
kiranzaman <kiran.k.zaman@gmail.com>
2017-05-09 05:34:30 +0500

    Update sjf.py

--- sjf.py
+++ sjf.py
@@ -1,60 +1,73 @@
 import time
+#function to read data from the file 
 def data_read():
-	co=0
+	total_process=0
+	#read data separated by white space 
 	line=f.readline().split()
 	while line:
-		co=co+1	
-		d = {}
-		d[line[-6]]=line[-5]
-		d[line[-4]]=line[-3]
-		d[line[-2]]=line[-1]
-		di.append(d)
+		total_process=total_process+1	
+		#dictionary to store data i.e., process number arrival time and burst time 
+		dictionary = {}
+		dictionary[line[-6]]=line[-5]
+		dictionary[line[-4]]=line[-3]
+		dictionary[line[-2]]=line[-1]
+		dict.append(dictionary)
 		line=f.readline().split()
-	return co
+	f.close()
+	#returns the count of total number of process 
+	return total_process
+#function to print the output 
 def print_func():
-	v=0
+	numberOfProcesses=0
 	print "process   turn around time   waiting time"
-	while v<count:
-		print proc[v] ,"		",turn_ar[v],"		", wait[v]
-		v=v+1
-
+	#loop till total number of processes 
+	while numberOfProcesses<count:
+		print process[numberOfProcesses],"		",turn_around[numberOfProcesses],"		", wait[numberOfProcesses]
+		numberOfProcesses=numberOfProcesses+1
+#main function 
 if __name__ == "__main__" :
 	f=open('file.txt')
 	i=0
-	ct=0
+	current_time=0
 	st=0
 	c1=0
 	j=0
 	ind=0
 	k=0
-	di =[]
+	dict =[]
 	q=[]
-	turn_ar= []
+	turn_around= []
 	wait = []
-	proc = []
+	process = []
 	count=data_read()
 	print count
-	nlist=sorted(di, key = lambda k: k['bt'])
+	#sort the list with respect to their burst time as sjf proceeds according to the shortest job(BT)
+	nlist=sorted(dict, key = lambda k: k['bt'])
 	while i < count:
 		check = False
 		q = sorted(nlist, key = lambda k: int(k["bt"]))
-		#print q
 		j=0
 		nlist = q
 		while check == False and j<count :
-			if (int(q[j]["at"]) < ct or int(q[j]["at"]) == ct):
-				ind = j
+			if (int(q[j]["at"]) < current_time or int(q[j]["at"]) == current_time):
+				process_entered = j
 				check = True
 			j=j+1
-		tim=int(q[ind]["bt"])
-		print(q[ind]["pn"]), "execution started"
-		print "now i'm going to execute for ",tim," seconds."
-		time.sleep(tim)
-		print "execution completed"
-		wait.append(ct-int(q[ind]["at"]))
-		ct =ct + int(q[ind]["bt"])		
-		turn_ar.append(ct-int(q[ind]["at"])) 
-		proc.append(q[ind]["pn"])
-		nlist[ind]["bt"]=str(100)
+		time1=int(q[process_entered]["bt"])
+		#once start , the process will execute to completion as sjf is non premptive 
+		print(q[process_entered]["pn"]), "execution started"
+		print "Process is going to execute for ",time1," seconds."
+		#nothing else will be done untill this process is in execution 
+		time.sleep(time1)
+		print "Execution completed"
+		#update waiting time 
+		wait.append(current_time-int(q[process_entered]["at"]))
+		current_time =current_time + int(q[process_entered]["bt"])
+		#TOT=FT-AT
+		#here finish time is the current time 		
+		turn_around.append(current_time-int(q[process_entered]["at"])) 
+		process.append(q[process_entered]["pn"])
+		nlist[process_entered]["bt"]=str(100)
 		i=i+1
+	#call print function to print the turn around time waiting time and process number 
 	print_func()
